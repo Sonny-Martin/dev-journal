8af7e55ff4bfa9bc79c83312bc089c349104062f
Sonny-Martin <Atarangisonny@gmail.com>
2025-04-09 21:02:19 +1200

    Add better calculator

---
+++ better_calculator.py
@@ -0,0 +1,18 @@
+print("Welcome to the Python Calculator!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose an operation (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    result = "Invalid operation"
+    
+print("Result:", result)
