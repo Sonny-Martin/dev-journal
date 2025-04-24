6014fbede4ff922c79139546f8db8121abb87a7c
Sonny-Martin <Atarangisonny@gmail.com>
2025-04-24 23:00:24 +1200

    Remove old Python files and start fresh

--- age_ calculator.py
+++
@@ -1,3 +0,0 @@
-age = int(input("how old are you? " ))
-age_in_5_years = age + 5
-print(f"In 5 years you will be {age_in_5_years} years old.")
--- countdown_timer.py
+++
@@ -1,9 +0,0 @@
-import time
-
-countdown_start = int(input("Enter the time for countdown in seconds: "))
-
-for i in range(countdown_start, 0, -1):
-    print(i)
-    time.sleep(1)  # Wait for 1 second after each number
-
-print("Time's up!")
