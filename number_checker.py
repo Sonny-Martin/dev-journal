6ba3c6f6c46108273253dfb0d8d182cea52f198c
Sonny Martin <atarangisonny@gmail.com>
2025-04-24 22:44:52 +1200

    Delete number_checker.py
    _Cleaning up old files to restart fresh with my own code - April 2025_

--- number_checker.py
+++
@@ -1,7 +0,0 @@
-number = float(input("Enter a number: "))
-if number > 0:
-   print ("The number is positive.")
-elif number < 0:
-   print ("The number is negitive.")
-else:
-   print("The number is zero.")
